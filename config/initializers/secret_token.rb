4fde0f6f1d796a2193de6044c16b68c5b04aeffb
nurulshuvo <shuvo@e-solutionpark.com>
2013-06-28 17:40:37 +0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jsinstorebd::Application.config.secret_token = 'db9f41aa9502e314292647d3246f647958a9ff62221a840004a97a2ae39182834b0184bcd3cddfba335a1d9e6a72a6753e5b00ec32036a65fcfa993977ce6ffc'
