6ab9a37ad6cd0dc83b21d7d1cdc32d335e718c29
nurulshuvo <shuvo@e-solutionpark.com>
2013-06-29 11:46:24 +0600

    added some validation to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :price, :title
+  validates :title, :description, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
