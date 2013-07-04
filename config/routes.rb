5a6570b9ec53ea96d6398dece88ffbd148687de9
nurulshuvo <shuvo@e-solutionpark.com>
2013-07-04 23:03:16 +0600

    created the cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Jsinstorebd::Application.routes.draw do
+  resources :orders
+
+
   resources :line_items
 
 
@@ -7,7 +10,9 @@ Jsinstorebd::Application.routes.draw do
 
   # get "store/index"
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
 root to: 'store#index', as: 'store'
   # The priority is based upon order of creation:
