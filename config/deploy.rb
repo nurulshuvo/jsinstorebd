01745b75e34afe5906adf97b78cbe92a6021a394
nurulshuvo <shuvo@e-solutionpark.com>
2013-10-07 23:03:34 +0600

    pushing to server

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, "jsinstorebd"
 set :user, "shuvo"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
-set :use_sudo, false
+set :use_sudo, true
 
 set :scm, "git"
 set :repository, "git@github.com:nurulshuvo/#{application}.git"
