1ecb6c32eb25ab1292140c357db835d49127688f
nurulshuvo <shuvo@e-solutionpark.com>
2013-11-29 00:01:53 +0600

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
   include FacebookShare
+
+  def page_title(title)
+    b_title = "eStore"
+    if title.empty?
+      b_title
+    else
+      "#{b_title} - #{title}"
+    end
+  end
 end
